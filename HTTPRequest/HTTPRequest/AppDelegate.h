3090d77cac11e1e97d73ba22e09829afdb63f191
Jason Yang <ja2yang@nero.com>
2013-07-18 17:24:20 +0800

    init project HTTPRequest

---
+++ HTTPRequest/HTTPRequest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HTTPRequest
+//
+//  Created by Jason Yang on 13-7-18.
+//  Copyright (c) 2013年 nero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
